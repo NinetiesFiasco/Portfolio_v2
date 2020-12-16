1dbe8c43c9da105cc1ab6cda5154800a15cc6da1
Середин Антон <55971481+NinetiesFiasco@users.noreply.github.com>
2020-12-16 21:36:33 +0300

    Updates

---
+++ precache-manifest.bc097132c684d68fdadb51759126b145.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28105f6e13c0ef3695fce1ef38da4ed",
+    "url": "/Portfolio_v2/index.html"
+  },
+  {
+    "revision": "1d74b926ae487d1d85e5",
+    "url": "/Portfolio_v2/static/css/main.ec4f341f.chunk.css"
+  },
+  {
+    "revision": "6805ad1d31c25734e0a3",
+    "url": "/Portfolio_v2/static/js/2.dc5916dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio_v2/static/js/2.dc5916dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d74b926ae487d1d85e5",
+    "url": "/Portfolio_v2/static/js/main.2deef4a3.chunk.js"
+  },
+  {
+    "revision": "cb6a84e5461af30b8eff",
+    "url": "/Portfolio_v2/static/js/runtime-main.0f32297c.js"
+  },
+  {
+    "revision": "0c47c769fca67a71dc80eca364da8be6",
+    "url": "/Portfolio_v2/static/media/bg.0c47c769.png"
+  },
+  {
+    "revision": "3462e719ddbd241eb4c936af990c6f3e",
+    "url": "/Portfolio_v2/static/media/bg.3462e719.png"
+  },
+  {
+    "revision": "395f6b05ed6373f8633ed67d391ed52c",
+    "url": "/Portfolio_v2/static/media/bg.395f6b05.png"
+  },
+  {
+    "revision": "54dd68bd7866d82ef712a201910a918c",
+    "url": "/Portfolio_v2/static/media/bg.54dd68bd.png"
+  },
+  {
+    "revision": "2e7112788b9ed3de435c8e714b53a9d4",
+    "url": "/Portfolio_v2/static/media/myArchangelsk.2e711278.ttf"
+  }
+]);
